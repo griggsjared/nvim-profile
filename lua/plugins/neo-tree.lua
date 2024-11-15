909f85e5e3cb33cf4d212e02c94f2477513dfce9
Jared Griggs <griggsjared@gmail.com>
2024-11-15 13:25:26 -0700

    update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,26 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
+  opts = {
+  },
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    require("neo-tree").setup({
+       filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            -- add extension names you want to explicitly exclude
+            '.git',
+            '.DS_Store',
+            'thumbs.db',  
+          },
+          never_show = {}
+        },
+      },
+    })
+    vim.keymap.set("n", "<C-n>", "<CMD>Neotree toggle<CR>", {})   
 	end,
 }
